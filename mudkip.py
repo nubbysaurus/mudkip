45fd79550d7980ac6b5695138f4f024746081313
Jake Lee <jlee211@ucsc.edu>
2024-12-16 10:29:11 -0800

    v1 - submitted as assignment, but params need tuning for future use

--- mudkip.py
+++ mudkip.py
@@ -2,11 +2,11 @@
 @file   mudkip.py
 
 Based on input a given field configuration and hourly weather data, optimize the
-input of water by an irrigation system such that the total available water 
+input of water by an irrigation system such that the available groundwater 
 volume remains greater than a given level throughout the day.
 
 @author Jake Lee/nubby  (jlee211@ucsc.edu)
-@date   3 Dec 2024
+@date   13 Dec 2024
 """
 import csv
 import sys
@@ -21,6 +21,8 @@ import matplotlib.pyplot as plt
 _DEFAULT_FARM_DATA_PATH = "./data/sample_farm_data.csv"
 _STEEP_FARM_DATA_PATH = "./data/sample_steep_data.csv"
 _DEFAULT_WEATHER_DATA_PATH = "./data/sample_weather_data.csv"
+_DEFAULT_SCHEDULE_PATH = "./data/sample_flat_schedule.csv"
+_STEEP_SCHEDULE_PATH = "./data/sample_steep_schedule.csv"
 _DEFAULT_DURATION = 24   # Hours
 
 # Datatypes.
@@ -69,7 +71,7 @@ class Farm(object):
     """
     def __init__(self, data: list[dict]):
         self.F = [] 
-        self.A = None
+        self.Q = None
         self.size = len(data)
         self._build_farm(data)
 
@@ -90,12 +92,12 @@ class Farm(object):
         [self.F.append(Field(field_data)) for field_data in data]
         # Create an associative, diagonally-symmetric matrix.
         n = len(data)
-        self.A = np.full((n,n), 0)
+        self.Q = np.zeros((n,n), dtype="float")
         self.update()
         print("    DONE.")
 
-    def _calculate_dV(self, i: int, j: int):
-        """_calculate_dV(i,j)
+    def _calculate_Q_ij(self, i: int, j: int):
+        """_calculate_Q_ij(i,j)
         Calculate the change in water volume at node i due to node j.
         """
         Fi = self.F[i]
@@ -117,9 +119,9 @@ class Farm(object):
         if (Fi.S != Fj.S):
             S_ij += (1/2) * abs(Fi.S - Fj.S) * d_horiz
         
-        # Return dV.
-        dV = -dK * S_ij * (Fi.H - Fj.H) / L_ij
-        return float(dV)
+        # Return Q_ij.
+        Q_ij = -dK * S_ij * (Fi.H - Fj.H) / L_ij
+        return Q_ij
 
     def update(self):
         """update()
@@ -130,9 +132,9 @@ class Farm(object):
         for i in range(n):
             for j in range(n):
                 if (i == j):
-                    self.A[i][j] = 0
+                    self.Q[i, j] = 0
                 else:
-                    self.A[i][j] = self.A[i][j] + self._calculate_dV(i, j)
+                    self.Q[i, j] = self.Q[i, j] + self._calculate_Q_ij(i, j)
 
 
 class Field(object):
@@ -165,7 +167,7 @@ class Field(object):
             # Set up field params.
             self.K = 1
             self.H = 0
-            self.S = 0    # Surface water depth at node.
+            self.S = 1    # Surface water depth at node.
             self._build_field(data)
         except KeyError as e:
             raise(e)
@@ -239,7 +241,8 @@ class Field(object):
               at least a small value. Also, without a small value, all water
               flow between nodes immediately becomes 0. <-- TODO
         """
-        self.S = 0.001
+        # Pretty sure S is the surface area of the Field.
+        self.S = 1
 
 # Initialize H and K.  self._update_H() self._update_K()
     def irrigate(self, duration: float, rate: float):
@@ -264,6 +267,14 @@ def _load_csv(fpath: str) -> list[dict]:
             f"WARNING!! {fpath} is an empty file!"
         )
     return data
+
+def _write_csv(fpath: str, data: list):
+    print(f"Writing to {fpath}...")
+    with open(fpath, "w+") as csvs:
+        writer = csv.writer(csvs, delimiter=",")
+        for row in data:
+            writer.writerow(row)
+    print(f"    DONE")
 
 
 # Main functionality.
@@ -301,11 +312,11 @@ def _subplot_config(f, i: int, n: int, u, V_g, V_s):
     ax1.plot(
         V_g[index, :].value + V_s[index, :].value, c="c"
     )
-    ax1.set_ylabel(f"$(V_t)_{i}$", fontsize=16, c="c")
+    ax1.set_ylabel(f"$V(t)_{i}$", fontsize=16, c="c")
     ax1.set_xticks([])
     ax2 = ax1.twinx()
     ax2.plot(u[index, :].value, c="r")
-    ax2.set_ylabel(f"$(u_t)_{i}$", fontsize=16, c="r")
+    ax2.set_ylabel(f"$u(t)_{i}$", fontsize=16, c="r")
     return ax1
     
 
@@ -315,11 +326,47 @@ def plot_results(n, u, V_g, V_s):
     f = plt.figure(figsize=(10,8))
 
     ax1 = _subplot_config(f, 1, n, u, V_g, V_s)
-    ax1.set_title("V (L) and u (L/hr) vs Hour of Day", fontsize=20)
+    ax1.set_title("V (m^3) and u (m^3) vs Hour of Day", fontsize=20)
     
-    [_subplot_config(f, i, n, u, V_g, V_s) for i in range(2,n+1)]
+    [_subplot_config(f, i, n, u, V_g, V_s) for i in range(2,n)]
+
+    ax1 = _subplot_config(f, n, n, u, V_g, V_s)
+    ax1.set_xticks(range(0,len(u.value[0])+1))
 
     plt.show()
+
+def _format_schedule(data) -> dict[list[str]]:
+    time = lambda t: str(t).rjust(2,"0")+":00"
+    schedule = []
+    header = [
+        "Time",
+        "Field1",
+        "Field2",
+        "Field3",
+        "Field4",
+        "Field5",
+        "Field6",
+        "Field7",
+        "Field8",
+        "Field9"
+    ]
+    schedule.append(header)
+    for t in range(len(data[0])):
+        schedule.append([
+            time(t),
+            round(data[0][t], 2),
+            round(data[1][t], 2),
+            round(data[2][t], 2),
+            round(data[3][t], 2),
+            round(data[4][t], 2),
+            round(data[5][t], 2),
+            round(data[6][t], 2),
+            round(data[7][t], 2),
+            round(data[8][t], 2)
+        ]) 
+
+    return schedule
+
 
 def run_mudkip(farm: Farm, weather: list[dict]):
     """run_mudkip(farm)
@@ -329,66 +376,91 @@ def run_mudkip(farm: Farm, weather: list[dict]):
     @return schedule    (dict)  
     """
     n = len(farm.F)
+    Q = farm.Q
     T = _DEFAULT_DURATION
+    tdelta = T / 24
 
     et = []
 
     # Threshold volume needed at each Field; doubles as initial value.
-    alpha = np.full(n, 1)
+    alpha = np.full(n, 1, dtype="float")
     # u = input water at each Field node.
-    u = cp.Variable((len(farm.F), T))
-    # V_s and V_g are the surface and ground water volume at each node.
-    V_s = cp.Variable((len(farm.F), T + 1))
-    V_g = cp.Variable((len(farm.F), T + 1))
+    u = cp.Variable((n, T))
+    u_0 = np.full(n, 0, dtype="float") # Start without any irrigation for now.
+    u_max = np.full(n, 25, dtype="float")
+    et_0 = np.full(n, FAO(0, float(weather[0]["Temperature"])), dtype="float")
+    q_0 = u_0 + et_0
 
     # These parameters simulate the relative moisture retention factors of air
     # and soil.
-    sigma = 0.1     # Surface water evaporates quickly.
-    epsilon = 0.5   # Water stays longer in soil.
+    sigma = 0.3     # Surface water evaporates quickly.
+    epsilon = 0.6   # Water stays longer in soil.
  
-    V_s0 = np.full(n, 2)
-    V_g0 = np.full(n, 0)
+    V_s0 = np.full(n, 1)
+    V_g0 = np.full(n, 2)
+
+    V = cp.Variable((n, T + 1))
+    V_s = cp.Variable((n, T + 1))
+    V_g = cp.Variable((n, T + 1))
     
     cost = 0
     constr = []
 
+    # Calculate the sum(Q_i) array to describe water dynamics for each node.
+    sQ = []
+    for i in range(n):
+        Q_i = np.sum(Q[i])
+        sQ.append(Q_i)
+
+    # Add a water storage parameter to this to account for soil absorption.
+    # TODO(nubby): Bind to characteristics of each Field node type.
+    S = np.full(n, 0)
+
     for t in range(T):
         # Calculate the rate of evapotranspiration at t.
-        fao =  FAO(
-            t, float(weather[t]["Temperature"])
-        )
-        # Calculate the next iteration's surface water volume.
+        et =  FAO(t, float(weather[t]["Temperature"]))
         constr += [
-            V_s[:, t+1] == (
-                np.diag(np.full(n, 1)) - farm.A
-            ) @ (V_s[:, t] + u[:, t]) + np.full(n, (1-sigma) * fao)
-        ]
-        # Calculate the next iteration's ground water volume.
-        constr += [
-            V_g[:, t+1] == (
-                V_g[:, t] + np.diag(np.full(n, epsilon)) @ (
-                    V_s[:, t]
-                ) + np.full(n, (1-epsilon) * fao)
+            V[:, t+1] == V_s[:, t+1] + V_g[:, t+1],
+            V_g[:, t+1] == V_g[:, t] + epsilon * V_s[:, t] + (1-epsilon) * et,
+            V_s[:, t+1] == (1-epsilon) * V_s[:, t] + (1-sigma) * et + (
+                u[:, t] + Q @ V_s[:, t]
             )
         ]
-        constr += [V_g[:, t] + V_s[:, t] >= alpha]
-        constr += [u[:, t] >= 0]
+        constr += [V_g[:, t] >= alpha]
+        # Add the sum of the water inputs to the cost at each step.
         cost += cp.sum(u[:, t])
-    constr += [V_g[:, T] == V_g0, V_g[:, 0] == V_g0]
-    constr += [V_s[:, T] == V_s0, V_s[:, 0] == V_s0]
+        # TODO(nubby): The following constraints make the primal problem
+        #              unsolveable; try to reframe this using the Lagrangian
+        #              dual.
+        #constr += [u[:, t] >= 0]
+        constr += [u[:, t] >= 0, u[:, t] <= u_max]
+        #constr += [V_s[:, t] >= 0]
+    constr += [V[:, 0] == V_s0 + V_g0]
+    constr += [V_g[:, 0] == V_g0, V_s[:, 0] == V_s0]
+    constr += [V_g[:, T] == V_g0]
+    constr += [u[:, 0] == u_0]
 
     problem = cp.Problem(cp.Minimize(cost), constr)
     problem.solve()
 
     print("Problem status: ", problem.status)
     print("Total water used: ", problem.value)
-    print("Water volumes: ", V_g.value + V_s.value)
-    #print("Watering schedule: ", str(u[0].value))
-    #print("ET: ", str(et))
+    #print("Water volumes: ", V_g.value + V_s.value)
+    print("Watering schedule: ", str(u.value))
+
+    # Reporting to STDOUT.
+    av = 0
+    for i, s in enumerate(u.value):
+        print(i,". ",sum(s));
+        av += sum(s)
+    av /= len(u.value)
+    print("Average (m^3): ",av)
 
     plot_results(n, u, V_g, V_s)
 
-    return u.value
+    results = _format_schedule(u.value)
+
+    return results 
 
 def clean_up(e: Exception):
     print(f"{e}; good bye.")
@@ -397,8 +469,8 @@ def clean_up(e: Exception):
 if __name__ == "__main__":
     # Load location-identifiable data corresponding to a farm being studied.
     try:
-        #farm_data = _load_csv(_DEFAULT_FARM_DATA_PATH)
-        farm_data = _load_csv(_STEEP_FARM_DATA_PATH)
+        farm_data = _load_csv(_DEFAULT_FARM_DATA_PATH)
+        #farm_data = _load_csv(_STEEP_FARM_DATA_PATH)
     except OSError as e:
         clean_up(e)
 
@@ -418,3 +490,9 @@ if __name__ == "__main__":
     # Generate an optimal irrigation schedule for each Field in the simulation.
     watering_schedule = run_mudkip(farm, weather_data)
 
+    # Output a watering schedule for each node.
+    # NOTE: Watering "schedules" just describe the total volume of water that
+    #       (m^3) should be output by each sprinkler during the course of a
+    #       given hour to achieve optimal water efficiency for each node.
+    _write_csv(_DEFAULT_SCHEDULE_PATH, watering_schedule)
+    #_write_csv(_STEEP_SCHEDULE_PATH, watering_schedule)
